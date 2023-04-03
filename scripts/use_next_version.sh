36b415703893d179ff193758358859c2e57ee6a4
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2023-04-03 15:30:19 -0400

    use latest grist-core

--- scripts/use_next_version.sh
+++ scripts/use_next_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -ex
-aws s3 cp s3://grist-static/latest.js s3://grist-static/previous.js
-aws s3 cp s3://grist-static/next.js s3://grist-static/latest.js
+aws s3 cp s3://grist-static/latest.js s3://grist-static/previous.js --cache-control max-age=60
+aws s3 cp s3://grist-static/next.js s3://grist-static/latest.js --cache-control max-age=60
