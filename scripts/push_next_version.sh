d969a58cc179d1cc98ca62eb3ac800897305949c
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2023-08-17 12:48:50 -0400

    tweak deploy scripts for csv-viewer
    As a new top level entrypoint, a little bit more work is needed
    to issue something with a correct URL pointing to the versioned
    implementation, and to keep track of next and previous version.

--- scripts/push_next_version.sh
+++ scripts/push_next_version.sh
@@ -19,9 +19,11 @@ s3url="s3://grist-static/$version"
 rm -f dist/static/mocha.js dist/static/mocha.css
 # sync
 aws s3 sync dist/static $s3url
+
 # point to new version
 cat dist/latest.js | sed "s|XXX|'$http'|" > dist/latest-send.js
 aws s3 cp dist/latest-send.js s3://grist-static/next.js --cache-control max-age=60
 
 # same for csv viewer
-aws s3 cp dist/csv-viewer.js s3://grist-static/csv-viewer-next.js --cache-control max-age=60
+cat dist/csv-viewer.js | sed "s|XXX|'$http'|" > dist/csv-viewer-send.js
+aws s3 cp dist/csv-viewer-send.js s3://grist-static/csv-viewer-next.js --cache-control max-age=60
